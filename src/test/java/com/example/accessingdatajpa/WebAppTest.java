b8e38dec12e34e71eb149c377f38148c20f36cbf
wmajbour <wmajbour@hotmail.com>
2023-10-16 15:11:18 -0400

    YML Update

--- src/test/java/com/example/accessingdatajpa/WebAppTest.java
+++ src/test/java/com/example/accessingdatajpa/WebAppTest.java
@@ -1,14 +1,22 @@
 package com.example.accessingdatajpa;
 
+import org.apache.commons.collections4.functors.ExceptionPredicate;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class WebAppTest {
 
     @Value(value = "${local.server.port}")
@@ -16,6 +24,7 @@ public class WebAppTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
 
     @Autowired
     private MainController controller;
@@ -26,9 +35,48 @@ public class WebAppTest {
     }
 
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
-        assertThat(restTemplate.getForObject("http://localhost: " + port + "/",
-                String.class)).contains("List of Buddies");
+    public void getAddressBook() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost: " + port + "/",
+                String.class)).contains("[]");
+    }
+
+    @Test
+    public String createAddressBook() throws Exception{
+        String requestBody = "{}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+
+        String response = restTemplate.postForObject("http://localhost: " + port + "/createBook", request, String.class);
+        assertThat(response).contains("List of Buddies");
+        return response;
+
+    }
+
+    @Test
+    public void addBuddy() throws Exception{
+        String requestBody = "{\"name\": \"HR\", \"address\": \"942 Rotary Way\", \"phoneNumber\": \"613 513 5160\"}";
+        createAddressBook();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+
+        String response = restTemplate.postForObject("http://localhost: " + port + "/addBuddy?id=1", request, String.class);
+        assertThat(this.restTemplate.getForObject("http://localhost: " + port + "/book?id=1", String.class)).contains("HR");
+    }
+
+    @Test
+    public void deleteBuddy() throws Exception{
+        createAddressBook();
+        addBuddy();
+
+        this.restTemplate.delete("http://localhost: " + port + "removeBuddy?idbuddy=1&idbook=1", String.class);
+
+        assertThat(this.restTemplate.getForObject("http://localhost: " + port + "/books", String.class)).doesNotContain("HR");
     }
 
 }
