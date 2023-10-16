cbf0a947ea5a64e275d6c80a58e9b4089d64ae1e
wmajbour <wmajbour@hotmail.com>
2023-10-15 20:06:46 -0400

    Lab 5 update 2

--- src/test/java/com/example/accessingdatajpa/WebAppTest.java
+++ src/test/java/com/example/accessingdatajpa/WebAppTest.java
@@ -1,19 +1,34 @@
 package com.example.accessingdatajpa;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest
 public class WebAppTest {
 
+    @Value(value="${local.server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Autowired
     private MainController controller;
-
     @Test
     public void contextLoads() throws Exception{
         assertThat(controller).isNotNull();
     }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception{
+        assertThat(restTemplate.getForObject("http://localhost: " + port + "/",
+                String.class)).contains("List of Buddies");
+    }
+
 }
